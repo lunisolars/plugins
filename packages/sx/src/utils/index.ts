8b80b89ad770157b39105bb489522203121dbe4b
Natron Kan <waterbeside@163.com>
2023-04-04 20:53:46 +0800

    other(plugin-sx): 更改computeSolarTerm的返回结果

--- packages/sx/src/utils/index.ts
+++ packages/sx/src/utils/index.ts
@@ -1,8 +1,8 @@
 import { S_aLon, S_aLonT } from './xl'
 import { int2 } from './func'
 import { J2000, PI2 } from '../constants'
-import { JD } from './jd'
-import { SSQ } from './qs'
+import { JD, JDR } from './jd'
+// import { SSQ } from './qs'
 import { dtT } from './deltaT'
 
 const jqmc = new Array(
@@ -46,7 +46,17 @@ export const getMonthDayLength = (year: number, month: number, Bd0: number) => {
   return int2(JD.JD(year, month, 1 + ((0.1 / 60 + 0) / 60 + 12) / 24)) - J2000 - Bd0
 }
 
-export const computeSolarTerm = (date: Date) => {
+type ComputeSolarTermRes = {
+  index: number
+  d0: number
+  date: null | JDR
+  jd: number
+  jd2: number
+  timeStr: string
+  name: string
+}
+
+export const computeSolarTerm = (date: Date): ComputeSolarTermRes => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
   const day = date.getDate()
@@ -58,22 +68,20 @@ export const computeSolarTerm = (date: Date) => {
   //节气查找
   let w = S_aLon(jd2 / 36525, 3)
   w = (int2(((w - 0.13) / PI2) * 24) * PI2) / 24
-  const res = {
+  const res: ComputeSolarTermRes = {
     index: 0,
     d0: Bd0 + day - 1, //儒略日,北京时12:00
-    y: year,
-    m: month,
-    Lyear0: 0,
-    jqjd: 0, // 节气时刻
-    jqsj: '', // 节气时间串
-    jqmc: ''
+    date: null,
+    jd: 0, // 节气时刻对应的儒略日
+    jd2: 0, // 节气时刻对应的儒略日
+    timeStr: '', // 节气时间串
+    name: ''
   }
   let d: number
 
   //纪日,2000年1月7日起算
   let D = res.d0 - 6 + 9000000
 
-  console.log('d', qiAccurate(w))
   do {
     d = qiAccurate(w)
     D = int2(d + 0.5)
@@ -81,17 +89,18 @@ export const computeSolarTerm = (date: Date) => {
     const xn = int2((w / PI2) * 24 + 24000006.01) % 24
     console.log(xn, d, D, Bdn, res.d0)
     // lunisolar的节气序号从小寒气，所以要-1
-    res.index = (xn + 23) / 24
+    res.index = (xn + 23) % 24
     w += PI2 / 24
-    if (D >= Bd0 + Bdn) break
+    if (D > res.d0) break
     if (D < Bd0) continue
-
-    console.log(jqmc[xn])
-    res.jqmc = jqmc[xn] //取得节气名称
-
-    res.jqjd = d
-    res.jqsj = JD.timeStr(d)
-    if (D - Bd0 === day - 1) break
+    if (D === res.d0) {
+      res.name = jqmc[xn] //取得节气名称
+      res.jd = d
+      res.jd2 = 2451545 + d
+      res.date = JD.DD(res.jd2)
+      res.timeStr = JD.timeStr(d)
+      break
+    }
   } while (D + 12 < Bd0 + Bdn)
   return res
 }
