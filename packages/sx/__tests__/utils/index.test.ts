7dd799a421356249107b8834b28ad2e19fa01c02
Natron Kan <waterbeside@163.com>
2023-04-04 20:54:26 +0800

    test(plugin-sx): 补充测试用例

--- packages/sx/__tests__/utils/index.test.ts
+++ packages/sx/__tests__/utils/index.test.ts
@@ -2,5 +2,17 @@ import { computeSolarTerm } from '../../src/utils/'
 import lunisolar from 'lunisolar'
 
 describe('test computeSolarTerm', () => {
-  expect(computeSolarTerm(lunisolar('2023-04-05').toDate())).toBe({})
+  it('2023-04-05', () => {
+    const stRes = computeSolarTerm(lunisolar('2023-04-05').toDate())
+    expect(stRes.timeStr).toBe('09:13:03')
+  })
+  it('2023-04-20', () => {
+    const stRes = computeSolarTerm(lunisolar('2023-04-20').toDate())
+    console.log('stRes', stRes)
+    expect(stRes.timeStr).toBe('16:13:36')
+  })
+  it('2023-04-19', () => {
+    const stRes = computeSolarTerm(lunisolar('2023-04-19').toDate())
+    expect(stRes.timeStr).toBe('')
+  })
 })
